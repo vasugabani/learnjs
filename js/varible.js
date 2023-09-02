6c5cf0b08beb1ed6b1a7389568cd303e9aaacc93
intro <vasugabani1399@gmail.com>
2023-09-02 10:19:20 +0530

    varible

--- js/varible.js
+++ js/varible.js
@@ -1,13 +1,28 @@
-var x = 10;
-var x = 20;
+// var x = 10;
+// var x = 20;
 
-console.log(x);
+// console.log(x);
 
-var x;
-console.log(x);
+// var x;
+// console.log(x);
 
-var x = 10;
-{
-    var x = 20;
-}
+// var x = 10;
+// {
+//     var x = 20;
+// }
+// console.log(x);
+
+// let x = 10;
+// let x = 20;
+// console.log(x);
+
+// let x = 10;
+// {
+//     let x = 20;
+// }
+// console.log(x);
+
+let x;
+x = 10;
+x = x+5;
 console.log(x);
