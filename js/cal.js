7d8b9f8d62bf349baa40294df38084529e971dc5
intro <vasugabani1399@gmail.com>
2023-09-02 10:56:25 +0530

    cal

---
+++ js/cal.js
@@ -0,0 +1,27 @@
+let x = parseInt(prompt("enter value of x:"));
+let y = parseInt(prompt("enter value of y:"));
+
+let op = parseInt(prompt("enter value of op:"));
+
+console.log(x,y,op);
+
+switch (op) {
+    case '+':
+        x=x+y;
+        console.log(x);
+        break;
+    case'-':
+        x=x-y;
+        console.log(x);
+        break;
+    case'*':
+        x=x*y;
+        console.log(x);
+        break;
+    case'/':
+        x=x/y;
+        console.log(x);
+        break;        
+    default:
+        break;
+}
