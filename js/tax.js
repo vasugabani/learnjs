0b08c36b99c267868d93e97b27727119ef6af1da
intro <vasugabani1399@gmail.com>
2023-09-05 11:16:38 +0530

    tax

--- js/tax.js
+++ js/tax.js
@@ -1,22 +1,53 @@
 function handlesubmit() {
+    event.preventDefault();
     // console.log(ok);
     let salary=parseInt(document.getElementById("salary").value);
     console.log("salary is :",salary);
 
-    let tax;
+    let s1=0,s2=0,s3=0,s4=0,s5=0,s6=0,total,income;
 
     if(salary<=300000){
-        console.log("no pay tax");
-    }else if(salary>300000 && salary<=600000){
-        tax=300000*0 + (salary-300000)*0.05;
-    }else if(salary>600000 && salary<=900000){
-        tax=300000*0 + 300000*0.05 + (salary-600000)*0.1;
-    }else if(salary>900000 && salary<=1200000){
-        tax=300000*0 + 300000*0.05 + 300000*0.1 + (salary-900000)*0.15;
-    }else if(salary>1200000 && salary<=1500000){
-        tax=300000*0 + 300000*0.05 + 300000*0.1 + 300000*0.15 + (salary-1200000)*0.2;
+        s1=300000*0;
+    }else if( salary<=600000){
+        s1=300000*0;
+        s2=(salary-300000)*0.05;
+    }else if(salary<=900000){
+        s1=300000*0;
+        s2=300000*0.05;
+        s3=(salary-600000)*0.1;
+    }else if(salary<=1200000){
+        s1=300000*0;
+        s2=300000*0.05;
+        s3=300000*0.1; 
+        s4=(salary-900000)*0.15;
+    }else if(salary<=1500000){
+        s1=300000*0;
+        s2=300000*0.05;
+        s3=300000*0.1; 
+        s4=300000*0.15;
+        s5=(salary-1200000)*0.2;
     }else if(salary>1500000){
-        tax=300000*0 + 300000*0.05 + 300000*0.1 + 300000*0.15 + 300000*0.2 + (salary-1500000)*0.3;
+        s1=300000*0;
+        s2=300000*0.05;
+        s3=300000*0.1; 
+        s4=300000*0.15;
+        s5=300000*0.2;
+        s6=(salary-1500000)*0.3;
     }
-    console.log("tax is :",tax);
+    console.log("tax is :",s1,s2,s3,s4,s5,s6);
+
+    total=s1+s2+s3+s4+s5+s6;
+
+    income=salary/12;
+
+    document.getElementById("slab-1").innerHTML=s1;
+    document.getElementById("slab-2").innerHTML=s2;
+    document.getElementById("slab-3").innerHTML=s3;
+    document.getElementById("slab-4").innerHTML=s4;
+    document.getElementById("slab-5").innerHTML=s5;
+    document.getElementById("slab-6").innerHTML=s6;
+
+    document.getElementById("total").innerHTML=total;
+
+    document.getElementById("income").innerHTML=income;
 }
