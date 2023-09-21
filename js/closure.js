28c257032e539e16df819d8e622e4cbd2b1d9ed2
intro <vasugabani1399@gmail.com>
2023-09-21 09:30:52 +0530

    hoist

---
+++ js/closure.js
@@ -0,0 +1,19 @@
+let age = 25
+
+const persnolinfo = (name) => {
+
+    let education='bca'
+
+    return allinfo = (salary) => {
+        console.log(age);
+        console.log(name);
+        console.log(education);
+        console.log(salary);
+    }
+}
+
+let res = persnolinfo('vasu');
+console.log(res);
+
+res(10000)
+res(20000)
---
+++ js/hoisting.js
@@ -0,0 +1,57 @@
+// x=10
+// console.log(x);              //10
+
+
+// "use strict"        //strict mode
+
+// x=3;
+// console.log(x);             //x is not defined
+
+
+// name();                      //vasu
+// function name() {
+//     console.log('vasu');
+// }
+
+
+// number(25);
+
+// function number(value) {
+//     console.log(value);           //25
+// }
+
+
+// var x;
+
+// console.log(x);          //undefined
+
+// x=10;
+
+
+// console.log(x);          //undefined
+
+// var x;
+
+// x=5;
+
+
+// console.log(y);          //cannot access
+
+// let y;
+
+// y=6;
+
+
+// let a;
+
+// console.log(a);             //undefined
+
+// a=7;
+
+
+// const x;
+
+// console.log(x);             //Missing initializer
+
+// x=4;
+
